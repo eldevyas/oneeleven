069e0d7ae1a7a8ebd74e06b96dd095df362f30e5
Yassine Chettouch <yassine_ct@pm.me>
2022-03-24 10:27:27 +0100

    Uploaded fills

--- app/src/component/mailing.jsx
+++ app/src/component/mailing.jsx
@@ -1,4 +1,11 @@
 import React, {Component} from 'react';
+import Fill1 from '../assets/svg/Background/Fill 1.svg'
+import Fill2 from '../assets/svg/Background/Fill 2.svg'
+import Fill3 from '../assets/svg/Background/Fill 3.svg'
+import Fill4 from '../assets/svg/Background/Fill 4.svg'
+import Fill5 from '../assets/svg/Background/Fill 5.svg'
+import Fill6 from '../assets/svg/Background/Fill 6.svg'
+
 
 const Texture = () => {
     return(
