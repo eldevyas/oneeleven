c01b47f406f7392f5ae912712693d7d70f191585
Yassine Chettouch <yassine_ct@pm.me>
2022-03-25 12:05:28 +0100

    jQuery Animation code updated.
    Used toggle class instead of pure CSS animations to be able of controlling the pseudo-elements of each animation frame.

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,8 +1,9 @@
 function CheckScroll() {
-    console.log(window.pageYOffset);
-    if (window.pageYOffset >= 1900) {
+    if (window.pageYOffset >= 1500) {
         $('.Algorithm').addClass('AlgorithmJ')
-            //INPUT
+
+        //INPUT
+
         $('.Schema .Input div').addClass('DIV')
 
         $('.Schema .Input .C1').addClass('C1J')
@@ -14,6 +15,71 @@ function CheckScroll() {
         $('.Schema .Input .C4').addClass('C4J')
 
         $('.Schema .Input .Generate').addClass('GenerateJ')
+
+        // Process
+
+        $('.Schema .Process .Algo .C1').addClass('C1J')
+
+        $('.Schema .Process .Algo .C2').addClass('C2J')
+
+        $('.Schema .Process .Algo .C3').addClass('C3J')
+
+        $('.Schema .Process .Algo .C4').addClass('C4J')
+
+
+
+        // Output
+
+        $('.Schema .Output .C1').addClass('C1J')
+
+        $('.Schema .Output .C2').addClass('C2J')
+
+        $('.Schema .Output .C3').addClass('C3J')
+
+        $('.Schema .Output .C4').addClass('C4J')
+
+        $('.Schema .Output .Result').addClass('ResultJ')
+    } else if (window.pageYOffset == 0) {
+
+        $('.Algorithm').removeClass('AlgorithmJ')
+
+        // INPUT
+
+        $('.Schema .Input div').removeClass('DIV')
+
+        $('.Schema .Input .C1').removeClass('C1J')
+
+        $('.Schema .Input .C2').removeClass('C2J')
+
+        $('.Schema .Input .C3').removeClass('C3J')
+
+        $('.Schema .Input .C4').removeClass('C4J')
+
+        $('.Schema .Input .Generate').removeClass('GenerateJ')
+
+        $('.Schema .Output div').removeClass('DIV')
+
+        // Process
+
+        $('.Schema .Process .Algo .C1').removeClass('C1J')
+
+        $('.Schema .Process .Algo .C2').removeClass('C2J')
+
+        $('.Schema .Process .Algo .C3').removeClass('C3J')
+
+        $('.Schema .Process .Algo .C4').removeClass('C4J')
+
+        // Output
+
+        $('.Schema .Output .C1').removeClass('C1J')
+
+        $('.Schema .Output .C2').removeClass('C2J')
+
+        $('.Schema .Output .C3').removeClass('C3J')
+
+        $('.Schema .Output .C4').removeClass('C4J')
+
+        $('.Schema .Output .Result').removeClass('ResultJ')
     }
 }
 
